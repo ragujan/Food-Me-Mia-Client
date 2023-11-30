cb510cfb364f86c15a48ec44516554e306924314
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2023-11-30 22:51:14 +0530

    add: FastFoodCategory enum is added

---
+++ app/src/main/java/com/rag/foodMeMia/domain/FastFoodCategory.java
@@ -0,0 +1,14 @@
+package com.rag.foodMeMia.domain;
+
+public enum FastFoodCategory {
+    BURGER,
+    PIZZA,
+    FRIED_CHICKEN,
+    TACOS,
+    SANDWICH,
+    HOT_DOG,
+    FRIES,
+    ICE_CREAM,
+    SODA,
+    SALAD,
+}
